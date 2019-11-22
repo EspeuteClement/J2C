3579edcaa06f104641519916dad6239af157fb9a
Espeute Clement <espeut.clement@gmail.com>
2019-11-22 22:31:52 +0100

    Editor for cards

--- Scenes/Card.gd
+++ Scenes/Card.gd
@@ -2,18 +2,25 @@ tool
 extends Node2D
 
 enum Dir {
-	Top,
 	Right,
+	Top,
+	Left,	
 	Down,
-	Left,
 	Count
 };
 
+export var card_name : String setget set_name;
+export(String, MULTILINE) var activation_text : String setget set_activation_text;
+export(String, MULTILINE) var effect_text : String setget set_effect_text;
 export(Array, int) var values : Array = [0,0,0,0] setget set_values;
 
 var held = false;
+onready var Labels = [$Right, $Top, $Left , $Down];
 
-onready var Labels = [$Top, $Right, $Down, $Left];
+onready var CardName = $CardName;
+onready var ActivationText = $ActivationText;
+onready var EffectText = $EffectText;
+
 
 func set_values(new_values : Array):
 	if (new_values.size() != 4):
@@ -21,11 +28,31 @@ func set_values(new_values : Array):
 	values = new_values;
 	update_card_numbers();
 	
+func set_name(new_value : String) :
+	card_name = new_value;
+	update_card_numbers();
+
+func set_activation_text(new_value : String) :
+	activation_text = new_value;
+	update_card_numbers();
+
+func set_effect_text(new_value : String) :
+	effect_text = new_value;
+	update_card_numbers();
+	
 func update_card_numbers():
 	if (!Labels || Labels.size() != 4):
 		return
 	for direction in range(0, Dir.Count):
 		Labels[direction].text = str(values[direction]);
+		
+	if (CardName):
+		CardName.text = card_name;
+		
+	if (ActivationText):
+		ActivationText.bbcode_text = activation_text;
+	if (EffectText):
+		EffectText.bbcode_text = effect_text;
 	update();
 	
 
